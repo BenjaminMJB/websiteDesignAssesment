4d9bd0fc3fc31123dd39f45f3240a41ccdf6c0ac
Benjamin Bedford <benjamin@bedfords.org.uk>
2023-03-25 22:16:53 +0000

    22:16 25/03/2023

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function closeNav() {
 	document.getElementById("mySideBar").style.width = "0";
 	document.getElementById("main").style.marginLeft= "0";
 }
+
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
