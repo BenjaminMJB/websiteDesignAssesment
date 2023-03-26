a47c58570f980a51446d9000adeeb081aaf7d17d
Benjamin Bedford <benjamin@bedfords.org.uk>
2023-03-26 20:45:17 +0100

    26/03/2023 20:45

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-function openNav() {
-	document.getElementById("mySideBar").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
+function sideBarNav() {
+	var x = document.getElementById("mySideBar");
+	var y = document.getElementById("main");
+	var z = document.getElementById("sideBarButton");
+	console.log("1");
+	if (z.innerHTML === "☰") {
+		x.style.width = "250px";
+		y.style.marginLeft = "250px";
+		z.innerHTML = "X";
+		console.log("2");
+	}
+	else {
+		x.style.width = "0px";
+		y.style.marginLeft = "0px";
+		z.innerHTML = "☰";
+		console.log("3");
+	}
 }
 
-function closeNav() {
-	document.getElementById("mySideBar").style.width = "0";
-	document.getElementById("main").style.marginLeft= "0";
-}
-
-function textShowHide() {
-	var x = document.getElementById("greatPyramidOfGizaPara");
-	var y = document.getElementById("greatPyramidOfGizaImg");
-	var z = document.getElementById("greatPyramidOfGizaButton");
+function textShowHide(para, img, but) {
+	var x = document.getElementById(para);
+	var y = document.getElementById(img);
+	var z = document.getElementById(but);
 	if (x.style.visibility === "visible") {
 		x.style.visibility = "hidden";
 		x.style.width = "0px";
 		x.style.height = "0px";
-		y.style.paddingRight = "25%";
+		y.style.marginRight = "25%";
 		z.innerHTML = "Show Text";
 	} 	
 	else {
 		x.style.visibility = "visible";
 		x.style.width = "46%";
 		x.style.height = "auto";
-		y.style.paddingRight = "0px";
+		y.style.marginRight = "0px";
 		z.innerHTML = "Hide Text";
 	}
 }
