835c032de8915cfaac4a855b0a4ef4e475ca5bbd
Benjamin Bedford <benjamin@bedfords.org.uk>
2023-03-26 17:19:00 +0100

    26/03/2023 17:18

--- script.js
+++ script.js
@@ -8,11 +8,27 @@ function closeNav() {
 	document.getElementById("main").style.marginLeft= "0";
 }
 
-function myFunction(id) {
-  var x = document.getElementById(id);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+function textShowHide() {
+	var x = document.getElementById("greatPyramidOfGizaPara");
+	if (x.style.visibility === "visible") {
+		x.style.visibility = "hidden";
+		x.style.width = "0px";
+		x.style.height = "0px";
+	} 	
+	else {
+		x.style.visibility = "visible";
+		x.style.width = "49%";
+		x.style.height = "auto";
+	}
+}
+
+function imgSizeChange() {
+	var x = document.getElementById("greatPyramidOfGizaImg");
+	var y = document.getElementById("greatPyramidOfGizaPara");
+	if (y.style.visibility === "hidden") {
+		x.style.width = "98%";
+	}
+	else {
+		x.style.width = "49%";
+	}
 }
